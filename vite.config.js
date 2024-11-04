20ba432ac374dee0f940d883c18baaa6c62dedcb
arrudafdc <arrudafdc@gmail.com>
2024-11-04 16:14:23 -0300

    fix base in viteConfig

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/agrocad.com.br/",
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
